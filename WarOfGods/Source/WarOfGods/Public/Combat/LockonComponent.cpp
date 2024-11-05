2737bd256169353ac74db83bf74064fd3956e1cd
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2024-11-05 15:20:56 -0500

    Adding Input Actions

---
+++ WarOfGods/Source/WarOfGods/Public/Combat/LockonComponent.cpp
@@ -0,0 +1,39 @@
+// White Tiger Studios 2024
+
+
+#include "LockonComponent.h"
+
+// Sets default values for this component's properties
+ULockonComponent::ULockonComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void ULockonComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+void ULockonComponent::StartLockon()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Lockon Started!"));
+}
+
+
+// Called every frame
+void ULockonComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
